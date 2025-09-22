53925c17f3b4c3e5e35c92e643745f94074f022e
josuechevez1998 <josuechevez977@gmail.com>
2025-09-21 20:29:22 -0600

    feat: create component

---
+++ src/app/pages/barbers.component/barbers.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-barbers.component',
+  imports: [],
+  templateUrl: './barbers.component.html',
+  styleUrl: './barbers.component.css'
+})
+export class BarbersComponent {
+
+}
