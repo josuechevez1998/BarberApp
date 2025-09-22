da57a558736a3f1ab8b538400db704eaf26b243d
josuechevez1998 <josuechevez977@gmail.com>
2025-09-21 20:34:49 -0600

    feat update sidebar

--- src/app/routes/dashboard.routes.ts
+++ src/app/routes/dashboard.routes.ts
@@ -1,6 +1,9 @@
 import { Routes } from "@angular/router";
 import { DashboardComponent } from "../layouts/dashboard.component/dashboard.component";
 import { HomeComponent } from "../pages/home.component/home.component";
+import { CustomersComponent } from "../pages/customers.component/customers.component";
+import { BranchesComponent } from "../pages/branches.component/branches.component";
+import { BarbersComponent } from "../pages/barbers.component/barbers.component";
 
 export const dashboardRoutes: Routes = [
 
@@ -11,6 +14,18 @@ export const dashboardRoutes: Routes = [
       {
         path: 'home',
         component: HomeComponent
+      },
+      {
+        path: 'customers',
+        component: CustomersComponent
+      },
+      {
+        path: 'branches',
+        component: BranchesComponent
+      },
+      {
+        path: 'barbers',
+        component: BarbersComponent
       },
       {
         path: '**',
